39b0d15e764c38129f3c8441973e2305d7b2935e
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-18 20:39:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,8 +15,8 @@ end
 
 get 'say/:number/:phrase' do
 @number = params[:number].to_i
-@phrase = params[:phrase]+" "
-(@phrase*@number).strip
+@phrase = params[:phrase]
+@phrase*@number
 end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
