26e89b5ff6b5120399a54bfe2e1c4d5850c2ef88
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-18 20:28:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
   @word3 = params[:word3]
   @word4 = params[:word4]
   @word5 = params[:word5]
-  "#{@word1}#{@word2}#{@word3}#{@word4}#{@word5}."
+  "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   
 end
 
